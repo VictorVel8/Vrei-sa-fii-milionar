dd776e6c0169a8d273c2f61c34150f6608352b08
VictorVel8 <victorvel801@gmail.com>
2020-02-23 16:30:11 +0200

    First updt

---
+++ Vrei sa fii Milionar 2/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Vrei_sa_fii_Milionar_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.intrebare = new System.Windows.Forms.Label();
+            this.raspuns1 = new System.Windows.Forms.Button();
+            this.raspuns2 = new System.Windows.Forms.Button();
+            this.raspuns3 = new System.Windows.Forms.Button();
+            this.raspuns4 = new System.Windows.Forms.Button();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bune = new System.Windows.Forms.Label();
+            this.gresite = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // intrebare
+            // 
+            this.intrebare.AutoSize = true;
+            this.intrebare.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.intrebare.Location = new System.Drawing.Point(219, 77);
+            this.intrebare.Name = "intrebare";
+            this.intrebare.Size = new System.Drawing.Size(140, 33);
+            this.intrebare.TabIndex = 0;
+            this.intrebare.Text = "Intrebare";
+            // 
+            // raspuns1
+            // 
+            this.raspuns1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raspuns1.Location = new System.Drawing.Point(94, 219);
+            this.raspuns1.Name = "raspuns1";
+            this.raspuns1.Size = new System.Drawing.Size(238, 85);
+            this.raspuns1.TabIndex = 1;
+            this.raspuns1.Text = "raspuns1";
+            this.raspuns1.UseVisualStyleBackColor = true;
+            this.raspuns1.Click += new System.EventHandler(this.raspuns1_Click);
+            // 
+            // raspuns2
+            // 
+            this.raspuns2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raspuns2.Location = new System.Drawing.Point(595, 220);
+            this.raspuns2.Name = "raspuns2";
+            this.raspuns2.Size = new System.Drawing.Size(238, 85);
+            this.raspuns2.TabIndex = 2;
+            this.raspuns2.Text = "raspuns2";
+            this.raspuns2.UseVisualStyleBackColor = true;
+            this.raspuns2.Click += new System.EventHandler(this.raspuns2_Click);
+            // 
+            // raspuns3
+            // 
+            this.raspuns3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raspuns3.Location = new System.Drawing.Point(94, 393);
+            this.raspuns3.Name = "raspuns3";
+            this.raspuns3.Size = new System.Drawing.Size(238, 85);
+            this.raspuns3.TabIndex = 3;
+            this.raspuns3.Text = "raspuns3";
+            this.raspuns3.UseVisualStyleBackColor = true;
+            this.raspuns3.Click += new System.EventHandler(this.raspuns3_Click);
+            // 
+            // raspuns4
+            // 
+            this.raspuns4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raspuns4.Location = new System.Drawing.Point(595, 393);
+            this.raspuns4.Name = "raspuns4";
+            this.raspuns4.Size = new System.Drawing.Size(238, 84);
+            this.raspuns4.TabIndex = 4;
+            this.raspuns4.Text = "raspuns4";
+            this.raspuns4.UseVisualStyleBackColor = true;
+            this.raspuns4.Click += new System.EventHandler(this.raspuns4_Click);
+            // 
+            // startbtn
+            // 
+            this.startbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startbtn.Location = new System.Drawing.Point(380, 277);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(186, 140);
+            this.startbtn.TabIndex = 5;
+            this.startbtn.Text = "Incepe jocul";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Raspunsuri bune: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Raspunsuri gresite: ";
+            // 
+            // bune
+            // 
+            this.bune.AutoSize = true;
+            this.bune.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bune.Location = new System.Drawing.Point(174, 30);
+            this.bune.Name = "bune";
+            this.bune.Size = new System.Drawing.Size(15, 16);
+            this.bune.TabIndex = 8;
+            this.bune.Text = "0";
+            // 
+            // gresite
+            // 
+            this.gresite.AutoSize = true;
+            this.gresite.Location = new System.Drawing.Point(177, 66);
+            this.gresite.Name = "gresite";
+            this.gresite.Size = new System.Drawing.Size(13, 13);
+            this.gresite.TabIndex = 9;
+            this.gresite.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 546);
+            this.Controls.Add(this.gresite);
+            this.Controls.Add(this.bune);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.raspuns4);
+            this.Controls.Add(this.raspuns3);
+            this.Controls.Add(this.raspuns2);
+            this.Controls.Add(this.raspuns1);
+            this.Controls.Add(this.intrebare);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label intrebare;
+        private System.Windows.Forms.Button raspuns1;
+        private System.Windows.Forms.Button raspuns2;
+        private System.Windows.Forms.Button raspuns3;
+        private System.Windows.Forms.Button raspuns4;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label bune;
+        private System.Windows.Forms.Label gresite;
+    }
+}
+
---
+++ Vrei sa fii Milionar 2/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vrei_sa_fii_Milionar_2
+{
+   
+
+    public partial class Form1 : Form
+    {
+        public clasa_joc joc = new clasa_joc();
+        public int indice_intrebare;
+        public int nr_bune, nr_gresite;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            raspuns1.Visible = false;
+            raspuns2.Visible = false;
+            raspuns3.Visible = false;
+            raspuns4.Visible = false;
+            intrebare.Text = "Bine ai venit la VREI SA FII MILIONAR";
+            joc.creare_joc();
+
+        }
+
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            
+            startbtn.Visible = false;
+            raspuns1.Visible = true;
+            raspuns2.Visible = true;
+            raspuns3.Visible = true;
+            raspuns4.Visible = true;
+            nr_bune = 0;
+            nr_gresite = 0;
+            indice_intrebare = 0;
+            punere_intrebare(0);
+           
+        }
+
+        private void sfarsit_joc()
+        {
+            raspuns1.Visible = false;
+            raspuns2.Visible = false;
+            raspuns3.Visible = false;
+            raspuns4.Visible = false;
+
+            if (nr_gresite > nr_bune)
+            {
+                intrebare.Text = "Din pacate ai gresit destul de mult.... Mai invata";
+            }
+            else
+                intrebare.Text = "Felicitari... Te descurci de minune";
+
+        }
+
+        private void punere_intrebare(int ind)
+        {
+            if (ind == 9)
+                sfarsit_joc();
+            else
+            {
+                joc.incepere_joc(ind);
+                bune.Text = nr_bune.ToString();
+                gresite.Text = nr_gresite.ToString();
+            }
+        }
+
+        private void raspuns2_Click(object sender, EventArgs e)
+        {
+            if (joc.intrebari[indice_intrebare].raspuns_bun == 2)
+                nr_bune = nr_bune + 1;
+            else
+                nr_gresite = nr_gresite + 1;
+
+            indice_intrebare = indice_intrebare + 1;
+            punere_intrebare(indice_intrebare);
+        }
+
+        private void raspuns3_Click(object sender, EventArgs e)
+        {
+            if (joc.intrebari[indice_intrebare].raspuns_bun == 3)
+                nr_bune = nr_bune + 1;
+            else
+                nr_gresite = nr_gresite + 1;
+
+            indice_intrebare = indice_intrebare + 1;
+            punere_intrebare(indice_intrebare);
+        }
+
+        private void raspuns4_Click(object sender, EventArgs e)
+        {
+            if (joc.intrebari[indice_intrebare].raspuns_bun == 4)
+                nr_bune = nr_bune + 1;
+            else
+                nr_gresite = nr_gresite + 1;
+
+            indice_intrebare = indice_intrebare + 1;
+            punere_intrebare(indice_intrebare);
+        }
+
+        private void raspuns1_Click(object sender, EventArgs e)
+        {
+            if (joc.intrebari[indice_intrebare].raspuns_bun == 1)
+                nr_bune = nr_bune + 1;
+            else
+                nr_gresite = nr_gresite + 1;
+
+            indice_intrebare=indice_intrebare+1;
+            punere_intrebare(indice_intrebare);
+
+
+        }
+    }
+
+    public class clasa_intrebare
+    {
+
+        public clasa_intrebare(string intreb, string r1, string r2, string r3, string r4, int rb)
+        {
+            intrebare = intreb;
+            raspuns1 = r1;
+            raspuns2 = r2;
+            raspuns3 = r3;
+            raspuns4 = r4;
+            raspuns_bun = rb;
+        }
+
+        public string intrebare;
+        public string raspuns1, raspuns2, raspuns3, raspuns4;
+        public int raspuns_bun;
+    }
+
+    public class clasa_joc
+    {
+        public  clasa_intrebare[] intrebari= new clasa_intrebare[10];
+
+
+        public void incepere_joc(int i)
+        {
+                Form1.ActiveForm.Controls["intrebare"].Text = intrebari[i].intrebare;
+                Form1.ActiveForm.Controls["raspuns1"].Text = intrebari[i].raspuns1;
+                Form1.ActiveForm.Controls["raspuns2"].Text = intrebari[i].raspuns2;
+                Form1.ActiveForm.Controls["raspuns3"].Text = intrebari[i].raspuns3;
+                Form1.ActiveForm.Controls["raspuns4"].Text = intrebari[i].raspuns4;
+      
+        }
+        
+        private void amestecare_intrebari()
+        {
+            Random rnd = new Random();
+            int ind;
+            clasa_intrebare aux;
+
+            for (int i=0; i<9; ++i)
+            {
+                ind = rnd.Next(0, 9);
+                aux = intrebari[ind];
+                intrebari[ind] = intrebari[i];
+                intrebari[i] = aux;
+            }
+        }
+
+        public void creare_joc()
+        {
+            intrebari[0] = new clasa_intrebare("Din ce fruct se face cidrul?", "Mar", "Capsune", "Strugure", "Para", 1);
+            intrebari[1] = new clasa_intrebare("Ce studiaza etimologia?", "Originea omului", "Originea universului", "Originea religilor", "Originea cuvintelor", 4);
+            intrebari[2] = new clasa_intrebare("Care este valoarea lui PI", "3.1412", "3.1415", "3.1425", "3,1465", 2);
+            intrebari[3] = new clasa_intrebare("Care este vecinul Romaniei din Nord-Est", "Republica Moldova", "Ukraina", "Bulgaria", "Ungaria", 1);
+            intrebari[4] = new clasa_intrebare("Care este capitala Indiei", "Mumbai", "Bollywood", "New Delhi", "Faridabad", 3);
+            intrebari[5] = new clasa_intrebare("In ce an a avut loc Marea Unire", "1919", "1917", "1920", "1918", 4);
+            intrebari[6] = new clasa_intrebare("Cate calorii are un McPuisor", "240", "320", "180", "400", 1);
+            intrebari[7] = new clasa_intrebare("Care este cel mai popular limbaj de programare", "C++", "C#", "Python", "Java", 4);
+            intrebari[8] = new clasa_intrebare("Ce inaltime are Lebron James", "2.00m", "1.98m", "2.06m", "2.15m", 3);
+
+            amestecare_intrebari();
+
+
+
+        }
+    }
+}
